885a67f32ebe16b7436ec3b76222e4ea7d98e35d
blokkinen <viktoria_blok@hotmail.com>
2016-08-09 16:09:24 +0200

    added requirements

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,4 +8,16 @@ class Atm
   def withdraw(amount)
     @funds -= amount
   end
+
+  def withdraw(amount, account)
+
+    case
+    when amount > account.balance then
+      return
+    else
+      @funds -= amount
+      account.balance = account.balance - amount
+      { status: true, message: 'success', date: Date.today, amount: amount }
+    end
+  end
 end
