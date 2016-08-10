d0ab6bb6e37b01d31f6a69ad348042f0ead16650
blokkinen <viktoria_blok@hotmail.com>
2016-08-10 10:31:42 +0200

    Small changes to make it more readble

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,19 +5,22 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount)
-    @funds -= amount
-  end
-
   def withdraw(amount, account)
-
     case
-    when amount > account.balance then
-      return
+    when insufficient_funds_in_account?(amount, account) then
+      { status: false, message: 'insufficient funds', date: Date.today }
     else
-      @funds -= amount
-      account.balance = account.balance - amount
-      { status: true, message: 'success', date: Date.today, amount: amount }
+      perform_transaction(amount, account)
     end
   end
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+ end
 end
